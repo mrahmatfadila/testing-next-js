dd84053e2f44bc120870b2b10cda5b2b615d15f4
Muhamad Rahmat Fadila <68174870+mrahmatfadila@users.noreply.github.com>
2024-02-07 23:42:53 +0700

    CHANGE NEXT CONFIG

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,13 @@
+// next.config.mjs
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  webpack5: true,
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false };
+
+    return config;
+  },
   reactStrictMode: true,
   images: {
     remotePatterns: [
